d8eb47ef7f61dd7d0bf44ffade5c942e852d22ad
Fernando Nieto <fernandonietop9@gmail.com>
2019-05-06 08:50:36 -0500

    Fix code style

--- DLinkedList.py
+++ DLinkedList.py
@@ -1,56 +1,56 @@
 import gc
 
 class Node:
-  def __init__(self, dataval= None):
-    self.dataval = dataval
-    self.nextval = None
-    self.prevval = None
+  def __init__(self, data_val= None):
+    self.data_val = data_val
+    self.next_val = None
+    self.prev_val = None
 
 class DoublyLinkedList:
   def __init__(self):
-    self.headval = None
+    self.head_val = None
 
   def print_list(self):
-    printval = self.headval
+    printval = self.head_val
     while printval is not None:
-      print(printval.dataval)
-      printval = printval.nextval
+      print(printval.data_val)
+      printval = printval.next_val
 
   # Add to the start
-  def add_first(self, newdata):
-    NewNode = Node(newdata)
-    NewNode.nextval = self.headval
-    if self.headval is not None:
-      self.headval.prevval = NewNode
-    self.headval = NewNode
+  def add_first(self, new_data):
+    new_node = Node(new_data)
+    new_node.next_val = self.head_val
+    if self.head_val is not None:
+      self.head_val.prev_val = new_node
+    self.head_val = new_node
 
   # Add to the end
-  def add_last(self, newdata):
-    NewNode = Node(newdata)
-    if self.headval is None:
-      self.headval = NewNode
+  def add_last(self, new_data):
+    new_node = Node(new_data)
+    if self.head_val is None:
+      self.head_val = new_node
       return
-    tail = self.headval
-    while tail.nextval:
-      tail = tail.nextval
-    NewNode.prevval = tail
-    tail.nextval = NewNode
+    tail = self.head_val
+    while tail.next_val:
+      tail = tail.next_val
+    new_node.prev_val = tail
+    tail.next_val = new_node
 
   def remove_first(self):
-    removeval = self.headval.nextval
-    if removeval:
-      self.headval = removeval
-      self.headval.prevval = None
+    remove_val = self.head_val.next_val
+    if remove_val:
+      self.head_val = remove_val
+      self.head_val.prev_val = None
     else:
-      self.headval = None
+      self.head_val = None
 
   def remove_last(self):
-    temp = self.headval
-    if temp.nextval:
-      while temp.nextval is not None:
+    temp = self.head_val
+    if temp.next_val:
+      while temp.next_val is not None:
         prev = temp
-        temp = temp.nextval
+        temp = temp.next_val
       temp.prev = None
-      prev.nextval = None
+      prev.next_val = None
     else:
-      self.headval = None
+      self.head_val = None
--- SLinkedList.py
+++ SLinkedList.py
@@ -1,51 +1,51 @@
 import gc
 
 class Node:
-  def __init__ (self, dataval=None):
-    self.dataval = dataval
-    self.nextval = None
+  def __init__ (self, data_val=None):
+    self.data_val = data_val
+    self.next_val = None
 
 class SLinkedList:
   def __init__(self):
-    self.headval = None
+    self.head_val = None
 
   # Print linked list
   def print_list(self):
-    printval = self.headval
+    printval = self.head_val
     while printval is not None:
-      print(printval.dataval)
-      printval = printval.nextval
+      print(printval.data_val)
+      printval = printval.next_val
 
   # Add to the start
-  def add_first(self, newdata):
-    NewNode = Node(newdata)
-    NewNode.nextval = self.headval
-    self.headval = NewNode
+  def add_first(self, new_data):
+    new_node = Node(new_data)
+    new_node.next_val = self.head_val
+    self.head_val = new_node
 
   # Add to the end
-  def add_last(self, newdata):
-    NewNode = Node(newdata)
-    if self.headval is None:
-      self.headval = NewNode
+  def add_last(self, new_data):
+    new_node = Node(new_data)
+    if self.head_val is None:
+      self.head_val = new_node
       return
-    tail = self.headval
-    while tail.nextval:
-      tail = tail.nextval
-    tail.nextval = NewNode
+    tail = self.head_val
+    while tail.next_val:
+      tail = tail.next_val
+    tail.next_val = new_node
 
   def remove_first(self):
-    removeval = self.headval.nextval
-    if removeval:
-      self.headval = removeval
+    remove_val = self.head_val.next_val
+    if remove_val:
+      self.head_val = remove_val
     else:
-      self.headval = None
+      self.head_val = None
 
   def remove_last(self):
-    temp = self.headval
-    if temp.nextval:
-      while temp.nextval is not None:
+    temp = self.head_val
+    if temp.next_val:
+      while temp.next_val is not None:
         prev = temp
-        temp = temp.nextval
-      prev.nextval = None
+        temp = temp.next_val
+      prev.next_val = None
     else:
-      self.headval = None
+      self.head_val = None
