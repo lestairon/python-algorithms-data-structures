8a12af8ef11376021c7f475eadc81dfa5ac95431
Fernando Nieto <fernandonietop9@gmail.com>
2019-05-08 08:59:20 -0500

    Change behavior if input is operator

--- src/stack/PostFixCalculator.py
+++ src/stack/PostFixCalculator.py
@@ -1,20 +1,16 @@
 from src.stack.StackArray import Stack
 
-class PostfixCalculator:
-  def calculate(self, *args):
-    stack = Stack()
-    for inputs in args:
-      if type(inputs) is int:
-        stack.push(inputs)
-        continue
-      r_value, l_value = stack.pop(), stack.pop()
-      if inputs == '+':
-        stack.push(l_value + r_value)
-      elif inputs == '-':
-        stack.push(l_value - r_value)
-      elif inputs == '*':
-        stack.push(l_value * r_value)
-      elif inputs == '/':
-        stack.push(l_value / r_value)
 
-    return stack.pop()
+class PostfixCalculator:
+    def calculate(self, *args):
+        stack = Stack()
+        operators = ['*', '/', '+', '-']
+        for inputs in args:
+            if type(inputs) is int:
+                stack.push(inputs)
+                continue
+            r_value, l_value = stack.pop(), stack.pop()
+            if inputs in operators:
+                stack.push(eval('l_value {} r_value'.format(inputs)))
+
+        return stack.pop()
